5d35e4c3fa3ac0be9cc4846b7f04b19d3d2ac049
Giovanni <giovanni.celestino@hotmail.com>
2026-08-24 22:18:07 -0300

    Uso dos parametros(Path, Query e Header)

--- secao03/main.py
+++ secao03/main.py
@@ -4,7 +4,10 @@ from fastapi import status
 from models import Curso
 from fastapi.responses import JSONResponse
 from fastapi import Response
-
+from fastapi import Path
+from fastapi import Query
+from fastapi import Header
+from typing import Optional
 #instancia o objeto
 app = FastAPI()
 
@@ -31,7 +34,7 @@ async def get_cursos():
 #Pega por individuo
 #Passo por parâmetro o ID do curso
 @app.get('/cursos/{curso_id}')
-async def get_curso(curso_id: int):
+async def get_curso(curso_id: int = Path(default=None, title='ID do curso', description='Deve ser entre 1 e 2', gt=0, lt=3)):
     try:
         curso = cursos[curso_id]
         #curso.update({"id": curso_id})
@@ -77,6 +80,19 @@ async def put_cursos(curso_id: int):
             status_code=status.HTTP_404_NOT_FOUND, detail=f'Não existe curso com o ID {curso_id}'
         )
 
+
+
+
+#Exemplo de Query Parameters
+#Informamos os valores no parametro do endpoint
+@app.get('/calculadora')
+async def calcular(a: int = Query(default=None, gt=5), b: int = Query(default=None, gt=10), x_geek: str = Header(default = None), c: Optional[int] = None):
+    soma: int = a + b
+    if c:
+        soma = soma + c
+
+    print (f'X-GEEK: {x_geek}')
+    return {"resultado": soma}
 
 
 
